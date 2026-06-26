5b289dcc8d4b639a7371d1cb531f0512fc9f38a5
Milad <moorara@users.noreply.github.com>
2026-06-26 15:03:26 -0400

    Update versions (#5)
    * Update Volt modules
    
    * Update container versions
    
    * Switch to built-in Fluent Forward client

--- observability/volt/go.mod
+++ observability/volt/go.mod
@@ -1,14 +1,14 @@
 module github.com/neatplatform/compose/observability/volt
 
-go 1.26.2
+go 1.26.4
 
 require (
-	github.com/fluent/fluent-logger-golang v1.10.1
 	github.com/golang/snappy v1.0.0
 	github.com/google/uuid v1.6.0
-	github.com/grafana/loki/pkg/push v0.0.0-20260527161227-7e76a647dcea
+	github.com/grafana/loki/pkg/push v0.0.0-20260624180310-4a3826309904
 	github.com/prometheus/client_golang v1.23.2
-	github.com/prometheus/prometheus v0.311.3
+	github.com/prometheus/prometheus v0.312.0
+	github.com/tinylib/msgp v1.6.4
 	go.opentelemetry.io/otel v1.44.0
 	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc v0.20.0
 	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.20.0
@@ -23,7 +23,7 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v1.44.0
 	go.opentelemetry.io/otel/trace v1.44.0
 	golang.ngrok.com/ngrok/v2 v2.1.4
-	golang.org/x/text v0.37.0
+	golang.org/x/text v0.38.0
 )
 
 require (
@@ -41,7 +41,6 @@ require (
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.67.5 // indirect
 	github.com/prometheus/procfs v0.16.1 // indirect
-	github.com/tinylib/msgp v1.3.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.44.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.10.0 // indirect
--- observability/volt/signal/forward.go
+++ observability/volt/signal/forward.go
@@ -5,23 +5,23 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
+	"sync"
 	"time"
 
-	"github.com/fluent/fluent-logger-golang/fluent"
 	"github.com/google/uuid"
+	"github.com/tinylib/msgp/msgp"
 )
 
 func runForward(args []string) {
 	fs := flag.NewFlagSet("forward", flag.ExitOnError)
-	jsonEnc := fs.Bool("json", false, "Use JSON encoding")
+	asJSON := fs.Bool("json", false, "Use JSON encoding")
 
 	if err := fs.Parse(args[1:]); err != nil {
 		fs.Usage()
 		os.Exit(1)
 	}
 
-	if *jsonEnc {
+	if *asJSON {
 		fmt.Println("Using JSON encoding")
 	} else {
 		fmt.Println("Using MessagePack encoding")
@@ -39,18 +39,20 @@ func runForward(args []string) {
 		"tenant", "test",
 	}
 
-	client, err := newFluentClient(addr, *jsonEnc)
+	client, err := newForwardClient("volt.logs", addr)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	defer client.Close()
 
 	for i, msg := range messages {
 		kvs := append(baseKV,
 			"uuid", uuid.NewString(),
 		)
 
-		if err := sendFluentLog(client, time.Now(), "info", msg, kvs...); err != nil {
+		if err := client.Send(time.Now(), "info", msg, kvs...); err != nil {
 			fmt.Println(err)
 		} else {
 			fmt.Printf("Sent log:  #%-2d  message=%s\n", i+1, msg)
@@ -60,50 +62,168 @@ func runForward(args []string) {
 	}
 }
 
-func newFluentClient(addr string, jsonEnc bool) (*fluent.Fluent, error) {
-	config := fluent.Config{
-		TagPrefix:          "volt",
-		Async:              true,
-		MarshalAsJSON:      jsonEnc,
-		SubSecondPrecision: !jsonEnc,
-	}
-
-	if addr != "" {
-		host, p, err := net.SplitHostPort(addr)
-		if err != nil {
-			return nil, fmt.Errorf("invalid address: %w", err)
-		}
-
-		port, err := strconv.Atoi(p)
-		if err != nil {
-			return nil, fmt.Errorf("invalid port: %w", err)
-		}
-
-		config.FluentHost, config.FluentPort = host, port
-	}
-
-	client, err := fluent.New(config)
-	if err != nil {
-		return nil, fmt.Errorf("error creating Fluent client: %w", err)
-	}
-
-	return client, nil
+func init() {
+	msgp.RegisterExtension(eventTimeExtType, func() msgp.Extension {
+		return new(eventTime)
+	})
 }
 
-func sendFluentLog(c *fluent.Fluent, t time.Time, level, message string, kv ...string) error {
-	record := make(map[string]any, 3+len(kv)/2)
+// eventTime is a nanosecond-precision timestamp encoded as msgpack extension type 0.
+// See https://github.com/fluent/fluentd/wiki/Forward-Protocol-Specification-v1#eventtime-ext-format
+type eventTime time.Time
+
+const (
+	eventTimeExtType = 0
+	eventTimeLen     = 8
+)
+
+func (t *eventTime) ExtensionType() int8 {
+	return eventTimeExtType
+}
+
+func (t *eventTime) Len() int {
+	return eventTimeLen
+}
+
+func (t *eventTime) MarshalBinaryTo(b []byte) error {
+	utc := time.Time(*t).UTC()
+
+	sec := uint32(utc.Unix())
+	nsec := uint32(utc.Nanosecond())
+
+	b[0], b[1], b[2], b[3] = byte(sec>>24), byte(sec>>16), byte(sec>>8), byte(sec)
+	b[4], b[5], b[6], b[7] = byte(nsec>>24), byte(nsec>>16), byte(nsec>>8), byte(nsec)
+
+	return nil
+}
+
+func (t *eventTime) UnmarshalBinary(b []byte) error {
+	if len(b) != eventTimeLen {
+		return fmt.Errorf("EventTime: invalid length %d", len(b))
+	}
+
+	sec := uint32(b[0])<<24 | uint32(b[1])<<16 | uint32(b[2])<<8 | uint32(b[3])
+	nsec := uint32(b[4])<<24 | uint32(b[5])<<16 | uint32(b[6])<<8 | uint32(b[7])
+
+	*t = eventTime(time.Unix(int64(sec), int64(nsec)))
+
+	return nil
+}
+
+// record represents a log record.
+type record map[string]string
+
+// entry represents a timestamped log record.
+type entry struct {
+	t eventTime
+	r record
+}
+
+// forwardClient is a TCP client for sending events/records to a Fluentd Forward Protocol endpoint.
+type forwardClient struct {
+	mu sync.Mutex
+
+	tag      string
+	endpoint string
+	conn     net.Conn
+}
+
+func newForwardClient(tag, endpoint string) (*forwardClient, error) {
+	c := &forwardClient{
+		tag:      tag,
+		endpoint: endpoint,
+	}
+
+	if err := c.connect(); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *forwardClient) connect() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	conn, err := net.DialTimeout("tcp", c.endpoint, 5*time.Second)
+	if err != nil {
+		return fmt.Errorf("error dialing connection: %w", err)
+	}
+
+	c.conn = conn
+
+	return nil
+}
+
+func (c *forwardClient) Send(t time.Time, level, message string, kv ...string) error {
+	r := make(record, 3+len(kv)/2)
 
 	// Add the fixed fields: timestamp, level, message
-	record["timestamp"] = t.Format(time.RFC3339Nano)
-	record["level"] = level
-	record["message"] = message
+	r["timestamp"] = t.Format(time.RFC3339Nano)
+	r["level"] = level
+	r["message"] = message
 
 	// Add the fields
 	for i := 0; i+1 < len(kv); i += 2 {
 		k, v := kv[i], kv[i+1]
-		record[k] = v
+		r[k] = v
 	}
 
-	// Send the record to the Forward endpoint.
-	return c.PostWithTime("test", t, record)
+	// No buffering for demoing purposes
+	entries := []entry{
+		{eventTime(time.Now()), r},
+	}
+
+	w := msgp.NewWriter(c.conn)
+
+	// Outer tuple: [tag, entries-array, option-map]
+	if err := w.WriteArrayHeader(3); err != nil {
+		return err
+	}
+
+	if err := w.WriteString(c.tag); err != nil {
+		return err
+	}
+
+	// Entries: array of [EventTime, record] tuples
+	if err := w.WriteArrayHeader(uint32(len(entries))); err != nil {
+		return err
+	}
+
+	for _, e := range entries {
+		if err := w.WriteArrayHeader(2); err != nil {
+			return err
+		}
+
+		if err := w.WriteExtension(&e.t); err != nil {
+			return err
+		}
+
+		if err := w.WriteMapStrStr(e.r); err != nil {
+			return err
+		}
+	}
+
+	// No options
+	if err := w.WriteMapHeader(0); err != nil {
+		return err
+	}
+
+	return w.Flush()
+}
+
+func (c *forwardClient) Close() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.conn != nil {
+		err := c.conn.Close()
+		c.conn = nil
+
+		if err != nil {
+			return fmt.Errorf("error closing connection: %w", err)
+		}
+	}
+
+	return nil
 }
